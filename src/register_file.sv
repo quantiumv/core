cf18673b3a4e291c093ba2ea823d135a707d8732
Siddh Raman Pant <25429745+siddhpant@users.noreply.github.com>
2023-11-04 15:56:51 +0530

    reg_file: Remove PC, dedicated RA and SP output, and standardise

--- src/register_file.sv
+++ src/register_file.sv
@@ -35,9 +35,6 @@
  * Output ports:
  *	o_read_gpr_A_data: Data read from the selected register #1.
  *	o_read_gpr_B_data: Data read from the selected register #2.
- *	o_program_counter: Data read from the program counter.
- *	o_return_address: Data read from the conventional ra register (r1).
- *	o_stack_pointer: Data read from the conventional sp register (r2).
  */
 module register_file #(
 	parameter num_regs = `REG_FILE_SIZE,
@@ -53,31 +50,10 @@ module register_file #(
 
 	input logic				i_load_gpr,
 	input logic	[(l2_num_regs - 1):0]	i_load_gpr_sel,
-	input logic	[(`WORD_SIZE - 1):0]	i_load_gpr_data,
-
-	input logic				i_load_pc,
-	input logic	[(`WORD_SIZE - 1):0]	i_load_pc_data,
-	output logic	[(`WORD_SIZE - 1):0]	o_program_counter,
-
-	output logic	[(`WORD_SIZE - 1):0]	o_return_address,
-	output logic	[(`WORD_SIZE - 1):0]	o_stack_pointer
+	input logic	[(`WORD_SIZE - 1):0]	i_load_gpr_data
 );
 	/* num_regs general purpose registers, each WORD_SIZE bit wide. */
 	logic [(`WORD_SIZE - 1):0]	gp_registers [0:(num_regs - 1)];
-
-
-	/*
-	 * r1 stores the return address (i.e., it is the link register), and
-	 * r2 stores the stack pointer, conventionally.
-	 * Refer page 14 of RISC-V spec.
-	 */
-	assign o_return_address = gp_registers[1];
-	assign o_stack_pointer = gp_registers[2];
-
-
-	/* Program counter is an additional register (ref. page 13, 14). */
-	logic [(`WORD_SIZE - 1):0] program_counter;
-	assign o_program_counter = program_counter;
 
 
 	/* Read at both ports (NB: r0 is hardwired to 0). */
@@ -87,19 +63,16 @@ module register_file #(
 
 	/* Write. */
 	always @(posedge i_clk) begin: write
-		/* r0 is to be hardwired to 0 (ref. page 13). */
-		gp_registers[0] <= 0;
-
 		/*
 		 * Load GPR.
 		 * NB: We don't write at r0. Thus, select must be non-zero.
 		 */
-		if (i_load_gpr && i_load_gpr_sel)
-			gp_registers[i_load_gpr_sel] <= i_load_gpr_data;
-
-		/* Load program counter. */
-		if (i_load_pc)
-			program_counter <= i_load_pc_data;
+		if (i_load_gpr_sel) begin: load_gpr
+			if (i_load_gpr)
+				gp_registers[0] <= 0;
+			else
+				gp_registers[i_load_gpr_sel] <= i_load_gpr_data;
+		end: load_gpr
 	end: write
 endmodule: register_file
 
